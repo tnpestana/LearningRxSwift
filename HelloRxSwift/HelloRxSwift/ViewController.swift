b05fa948d373915f43ea865d79f686a26ff204bf
Tiago Pestana <tnevespestana@gmail.com>
2020-02-12 21:49:15 +0000

    observables, subscriptions and disposables

--- HelloRxSwift/HelloRxSwift/ViewController.swift
+++ HelloRxSwift/HelloRxSwift/ViewController.swift
@@ -7,15 +7,12 @@
 //
 
 import UIKit
-import RxSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        _ = Observable.from([1, 2, 3, 4, 5])
     }
 
 
--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
+import RxSwift
 
-var str = "Hello, playground"
+let disposeBag = DisposeBag()
+
+Observable.of("a", "b", "c")
+    .subscribe {
+        print($0)
+}.disposed(by: disposeBag)
+
+Observable<String>.create { (observer) -> Disposable in
+    observer.onNext("a")
+    observer.onCompleted()
+    return Disposables.create()
+}.subscribe(onNext: { print($0) }, onError: { print($0) }, onCompleted: { print("completed") }, onDisposed: { print("disposed") })
+
