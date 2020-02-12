396fe2ad84e74ae31be324ff8879daa836235b63
Tiago Pestana <tnevespestana@gmail.com>
2020-02-12 20:35:57 +0000

    create RxSwift playground

--- HelloRxSwift/HelloRxSwift/ViewController.swift
+++ HelloRxSwift/HelloRxSwift/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        _ = Observable.from([1, 2, 3, 4, 5])
     }
 
 
