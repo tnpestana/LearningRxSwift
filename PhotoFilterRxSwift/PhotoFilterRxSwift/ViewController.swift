4253d949c7c63de68cf80f0ee659e2bf87335a90
Tiago Pestana <tnevespestana@gmail.com>
2020-02-16 21:14:34 +0000

    update presented image via subscription

--- PhotoFilterRxSwift/PhotoFilterRxSwift/ViewController.swift
+++ PhotoFilterRxSwift/PhotoFilterRxSwift/ViewController.swift
@@ -7,13 +7,29 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imgPhoto: UIImageView!
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard
+            let nav = segue.destination as? UINavigationController,
+            let photosCVC = nav.viewControllers.first as? PhotosCollectionViewController
+        else {
+            return
+        }
+        
+        photosCVC.selectedPhoto.subscribe(onNext: { [weak self] photo in
+            self?.imgPhoto.image = photo
+        }).disposed(by: disposeBag)
     }
 }
 
