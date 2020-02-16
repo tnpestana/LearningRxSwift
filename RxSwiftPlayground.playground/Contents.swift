b25e71daf17c64925da7f80c0a6041bfc40c2d08
Tiago Pestana <tnevespestana@gmail.com>
2020-02-16 19:11:27 +0000

    Publish Subjects

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -3,6 +3,8 @@ import RxSwift
 
 let disposeBag = DisposeBag()
 
+//MARK: Observables
+print("\n## observables ##")
 Observable.of("a", "b", "c")
     .subscribe {
         print($0)
@@ -14,3 +16,14 @@ Observable<String>.create { (observer) -> Disposable in
     return Disposables.create()
 }.subscribe(onNext: { print($0) }, onError: { print($0) }, onCompleted: { print("completed") }, onDisposed: { print("disposed") })
 
+//MARK: Subjects
+print("\n## subjects ##")
+let subject = PublishSubject<String>()
+subject.onNext("issue 1")
+subject.subscribe { (event) in
+    print(event)
+}
+subject.onNext("issue 2")
+subject.onNext("issue 3")
+subject.onCompleted()
+subject.onNext("issue 4")
