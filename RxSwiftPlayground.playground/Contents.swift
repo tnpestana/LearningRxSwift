be2cb3f25f6ff582cfb9b416cf7f20fa88e20107
Tiago Pestana <tnevespestana@gmail.com>
2020-02-16 19:22:00 +0000

    Behaviour Subjects

--- RxSwiftPlayground.playground/Contents.swift
+++ RxSwiftPlayground.playground/Contents.swift
@@ -4,7 +4,7 @@ import RxSwift
 let disposeBag = DisposeBag()
 
 //MARK: Observables
-print("\n## observables ##")
+print("\n## Observables ##")
 Observable.of("a", "b", "c")
     .subscribe {
         print($0)
@@ -16,14 +16,23 @@ Observable<String>.create { (observer) -> Disposable in
     return Disposables.create()
 }.subscribe(onNext: { print($0) }, onError: { print($0) }, onCompleted: { print("completed") }, onDisposed: { print("disposed") })
 
-//MARK: Subjects
-print("\n## subjects ##")
-let subject = PublishSubject<String>()
-subject.onNext("issue 1")
-subject.subscribe { (event) in
+//MARK: Publish Subjects
+print("\n## Publish Subjects ##")
+let publishSubject = PublishSubject<String>()
+publishSubject.onNext("issue 1")
+publishSubject.subscribe { (event) in
     print(event)
 }
-subject.onNext("issue 2")
-subject.onNext("issue 3")
-subject.onCompleted()
-subject.onNext("issue 4")
+publishSubject.onNext("issue 2")
+publishSubject.onNext("issue 3")
+publishSubject.onCompleted()
+publishSubject.onNext("issue 4")
+
+//MARK: Behaviour Subject
+print("\n## Behaviour Subjects ##")
+let behaviourSubject = BehaviorSubject(value: "Init value")
+behaviourSubject.onNext("last value")
+behaviourSubject.subscribe { (event) in
+    print(event)
+}
+behaviourSubject.onNext("issue 1")
