37fd6b62bb486516f8b25dee5be4dc247f7669ce
Tiago Pestana <tnevespestana@gmail.com>
2020-02-22 16:01:03 +0000

    import initial project for MVVM-C tutorial

---
+++ MVVM-C/MVVM-C/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM-C
+//
+//  Created by Tiago Pestana on 22/02/2020.
+//  Copyright © 2020 Tiago Pestana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
